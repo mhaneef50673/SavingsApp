21ed7e24b18c429a3fb6bbb9c7795579e500eec8
Mohamed Haneef <arvzteam@gmail.com>
2019-02-09 13:33:27 +0530

    Added x-axis label

--- App.js
+++ App.js
@@ -1,122 +1,126 @@
-import React from "react";
-import { View, Text, Dimensions, StyleSheet } from "react-native";
+import React from 'react'
+import { View, Text, Dimensions, StyleSheet } from 'react-native'
 
 export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       data: [
         {
-          name: "Classical music",
+          name: 'Jan',
           value: 100
         },
         {
-          name: "Alternative rock",
+          name: 'Feb',
           value: 200
         },
         {
-          name: "Pop",
+          name: 'Mar',
           value: 300
         },
         {
-          name: "Jazz",
+          name: 'Apr',
           value: 400
         },
-		{
-          name: "Jazz",
+        {
+          name: 'May',
           value: 200
         }
       ],
       xAxisPadding: 5,
-      chartWidth: Math.round(Dimensions.get("window").width) - 100,
+      chartWidth: Math.round(Dimensions.get('window').width) - 100,
       chartHeight: 300
-    };
+    }
   }
 
   renderLines = () => {
-    let maxValue = 0;    
+    let maxValue = 0
 
     this.state.data.forEach(element => {
-      maxValue = Math.max(maxValue, element.value);      
-    });
+      maxValue = Math.max(maxValue, element.value)
+    })
 
-    let gridValue = 0;
-    let gridScale = 100;
-    let gridLines = [];
+    let gridValue = 0
+    let gridScale = 100
+    let gridLines = []
     while (gridValue <= maxValue) {
-      var gridY = Math.round(this.state.chartHeight * (1 - gridValue / maxValue));
-      console.log(gridY);
-      gridValue += gridScale;
+      var gridY = Math.round(
+        this.state.chartHeight * (1 - gridValue / maxValue)
+      )      
+      gridValue += gridScale
       gridLines.push(
         <View
           key={gridValue}
           style={{
-            borderBottomColor: "red",
-            borderBottomWidth: StyleSheet.hairlineWidth,            
-            top: gridY,                                  
+            borderBottomColor: 'grey',
+            borderBottomWidth: 0.6,
+            top: gridY
           }}
-        >               
-        </View>
-      );
+        />
+      )
     }
 
-    return gridLines;
-  };
+    return gridLines
+  }
 
   renderBars = () => {
-    debugger;
-    const numOfBars = this.state.data.length;
-    const barSize = this.state.chartWidth / numOfBars - this.state.xAxisPadding;
+    const numOfBars = this.state.data.length
+    const barSize = this.state.chartWidth / numOfBars - this.state.xAxisPadding
 
-    let maxValue = 0;
+    let maxValue = 0
 
     this.state.data.forEach(element => {
-      maxValue = Math.max(maxValue, element.value);
-    });
+      maxValue = Math.max(maxValue, element.value)
+    })
 
     return this.state.data.map((categ, index) => {
       const barHeight = Math.round(
-        this.state.chartHeight * categ.value / maxValue
-      );
+        (this.state.chartHeight * categ.value) / maxValue
+      )
       return (
         <View
           key={index}
           style={{
-            backgroundColor: "blue",
+            backgroundColor: 'blue',
             marginLeft: 5,
-            marginBottom: 0,
             height: barHeight,
-            width: barSize,
-            zIndex: 100           
+            width: barSize
           }}
         />
-      );
-    });
-  };
-  render() {
+      )
+    })
+  }
+  render () {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <View
           style={{
             width: this.state.chartWidth,
-            height: this.state.chartHeight + 100,
+            height: this.state.chartHeight + 100
           }}
         >
-          <View style={{ flex: 1, flexDirection : 'column-reverse' }}>
-          {this.renderLines()}   
+          <View style={{ flex: 1, flexDirection: 'column-reverse' }}>
+            {this.renderLines()}
             <View
               style={{
                 flex: 1,
-                flexDirection: "row",
-                justifyContent: "flex-end",
-                alignItems: "flex-end"                
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                alignItems: 'flex-end'
               }}
             >
-              {this.renderBars()}              
-            </View>         
+              {this.renderBars()}
+            </View>            
           </View>
-        </View>        
+          <View style={{ flexDirection : 'row'}}>
+              <Text style={{ textAlign: 'center', marginLeft: 5, width : this.state.chartWidth / 5 - this.state.xAxisPadding}}>Jan</Text>
+              <Text style={{ textAlign: 'center', marginLeft: 5, width : this.state.chartWidth / 5 - this.state.xAxisPadding}}>Feb</Text>
+              <Text style={{ textAlign: 'center', marginLeft: 5, width : this.state.chartWidth / 5 - this.state.xAxisPadding}}>Mar</Text>
+              <Text style={{ textAlign: 'center', marginLeft: 5, width : this.state.chartWidth / 5 - this.state.xAxisPadding}}>Apr</Text>
+              <Text style={{ textAlign: 'center', marginLeft: 5, width : this.state.chartWidth / 5 - this.state.xAxisPadding}}>May</Text>
+          </View>
+        </View>
       </View>
-    );
+    )
   }
 }
